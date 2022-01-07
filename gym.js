37e3638b35e952d326319675ee7c9be3823d4666
mandoo2121 <49102049+mandoo2121@users.noreply.github.com>
2022-01-07 08:59:24 +0200

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head> 
+<meta charset="UT-8"/>
+<title>gym
+</title>
+<link rel="icon"  href="1.jpg"></link>
+<meta name="description" content="health is important for our health
+ and it give us alot as benefits so tha every person if 
+he wants his life to be better so get up and train"/>
+<meta name ="health" content="v1.0"/> 
+</head>
